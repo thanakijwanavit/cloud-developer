e6ce06a39026c7245d3f7d83dccf13aacc7ebb9c
nic <nwanavit@hatari.cc>
2020-03-09 22:18:29 +0700

    add appropriate permission to read dynamodb

--- course-04/exercises/lesson-2/sam_package/.aws-sam/build/HelloWorldFunction/index.js
+++ course-04/exercises/lesson-2/sam_package/.aws-sam/build/HelloWorldFunction/index.js
@@ -1,36 +1,23 @@
-const data = [
-    {
-        id: '1',
-        name: 'DOgs',
-        description: 'Only dog images here'
-    },
-    {
-        id: '2',
-        name: 'DOgs',
-        description: 'Only dog images here'
-    },
-    {
-        id: '3',
-        name: 'DOgs',
-        description: 'Only dog images here'
-    },
-    {
-        id: '4',
-        name: 'DOgs',
-        description: 'Only dog images here'
-    },
-]
+const AWS = require("aws-sdk")
 
+const docClient = new AWS.DynamoDB.DocumentClient()
 
+const groupsTable = process.env.GROUPS_TABLE
 
 exports.handler = async (event) => {
+    const result = await docClient.scan({
+        TableName: groupsTable
+    }).promise()
+
+    const items = result.Items
+
     return {
         statusCode : 200,
         headers: {
             'Access-Control-Allow-Origin': '*'
         },
         body: JSON.stringify({
-            items:data
+            items
         })
     }
 }
